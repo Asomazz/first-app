3c5c3fb8ed26931c183541d4d63bb2b8ce50605c
A <alzemamia@gmail.com>
2024-05-12 18:41:06 +0300

    done

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ function App() {
 
   return (
     <div>
-      <h1>
-        <center>{shopname}</center>
-      </h1>
-      <br />
-      <h3>
-        <center>{description}</center>
-      </h3>
-      <img
-        width="1500vw"
-        src="https://media.istockphoto.com/id/1218963825/photo/lovely-little-independent-coffee-shop.jpg?s=1024x1024&w=is&k=20&c=Ytmb-BFO-YPOJV-El-hxLFKclfsjkbWbPc_18h0BX70="
-      />
+      <center>
+        <h1>{shopname}</h1>
+        <br />
+        <h3>{description}</h3>
+        <img
+          width="1500vw"
+          src="https://media.istockphoto.com/id/1218963825/photo/lovely-little-independent-coffee-shop.jpg?s=1024x1024&w=is&k=20&c=Ytmb-BFO-YPOJV-El-hxLFKclfsjkbWbPc_18h0BX70="
+        />
+      </center>
     </div>
   );
 }
