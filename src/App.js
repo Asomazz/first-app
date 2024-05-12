795874b6a8e39125a40fd2583cfb3ae8d1b87a21
A <alzemamia@gmail.com>
2024-05-12 18:17:32 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const shopname = "coffeShop";
+  const description = "This a coffe shop";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        <center>{shopname}</center>
+      </h1>
+      <br />
+      <h3>
+        <center>{description}</center>
+      </h3>
+      <img
+        width="1500vw"
+        src="https://media.istockphoto.com/id/1218963825/photo/lovely-little-independent-coffee-shop.jpg?s=1024x1024&w=is&k=20&c=Ytmb-BFO-YPOJV-El-hxLFKclfsjkbWbPc_18h0BX70="
+      />
     </div>
   );
 }
